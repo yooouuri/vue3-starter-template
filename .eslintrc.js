694992eb341064d606da047b61fb1d336bcd958f
Youri van Mill <yourivanmill@gmail.com>
2022-01-23 00:28:30 +0100

    fix(lint): add missing typescript eslint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,16 @@ module.exports = {
     node: true
   },
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
     ecmaVersion: 2020
   },
+  parser: 'vue-eslint-parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended'
   ]
 }
